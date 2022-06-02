64723fa936cd83bf8bcb33bf1ad92a052b8fbd05
Toheeb <aladetoheeb5@gmail.com>
2022-06-02 22:24:58 +0100

    Task req

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,8 +7,8 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {:s}" .format(argv[1]))
     else:
-        print("{:d} arguments:".format(len(argv)))
-        for i in range(len(sys.argv)):
+        print("{:d} arguments:" .format(len(argv)))
+        for i in range(len(argv)):
             if i != 0:
-                print("{:d}: {:s}".format(i, argv[i]))
+                print("{:d}: {:s}" .format(i, argv[i]))
 
