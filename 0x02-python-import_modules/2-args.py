442b41184822a8517279ea42915176f30b5401b7
Toheeb <aladetoheeb5@gmail.com>
2022-06-02 22:59:04 +0100

    Hot fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
     if len(argv) == 1:
@@ -7,8 +6,8 @@ if __name__ == "__main__":
         print("1 argument:")
         print("1: {}".format(argv[1]))
     else:
-        print("{:d} arguments:".format(len(argv)))
+        print("{:d} arguments:".format(len(argv) -1))
         for i in range(len(argv)):
             if i != 0:
-                print("{:d}: {)".format(i, argv[i]))
+                print("{:d}: {}".format(i, argv[i]))
 
