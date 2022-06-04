4972c525b02f1c676d7b73765e1a44063e46ac53
Toheeb <aladetoheeb5@gmail.com>
2022-06-04 20:30:09 +0100

    fix pycodestyle

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,8 @@
 def new_in_list(my_list, idx, element):
     if my_list:
         new_list = my_list[:]
-        if (idx < 0 or idx > len(my_list) - 1):               return my_list
+        if (idx < 0 or idx > len(my_list) - 1):
+            return my_list
         for i in range(len(my_list)):
             if my_list[i] == my_list[idx]:
                 new_list[idx] = element
