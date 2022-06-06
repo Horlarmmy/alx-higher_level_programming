185d219fcb4f655761cd4d4af278991ad48e970f
Toheeb <aladetoheeb5@gmail.com>
2022-06-06 18:44:53 +0100

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,6 +4,6 @@ def print_matrix_integer(matrix=[[]]):
         for list in matrix:
             for i in range(len(list)):
                 if i != len(list) -1:
-                    print(list[i],end="")
+                    print(list[i],end=" ")
                 else:
                     print(list[i])
