bab82b3a817735b151c3d7458878fd6ffcb6eab1
Toheeb <aladetoheeb5@gmail.com>
2022-06-21 15:46:33 -0700

    Task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+Define class Square
+"""
+
+
+class Square:
+    """ class Square that defines the object by its size"""
+    def __init__(self, size=0):
+        """ Initializes the square object that store the size
+        Args:
+            param1 (int): size of the square
+        """
+        if type(size) != int:
+            raise TypeError("size must ne an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
