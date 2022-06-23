17bfb4c9fd51f80666939ebdc98f64c05e940860
Toheeb <aladetoheeb5@gmail.com>
2022-06-22 21:04:10 -0700

    Fix

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = my_list_2[:]
     for i in range(list_length):
@@ -16,9 +17,3 @@ def list_division(my_list_1, my_list_2, list_length):
             new_list[i] = 0
             pass
     return new_list
-
-
-my_1_1 = [10, 8, 4, 4]
-my_1_2 = [2, 0, "H", 2, 7]
-result = list_division(my_1_1, my_1_2, len(my_1_2))
-print(result)
--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def raise_execution():
     raise TypeError
-
-try:
-    raise_execution()
-except TypeError as te:
-    print("Exception rasied")
-
