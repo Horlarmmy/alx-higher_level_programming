f01da50aa812c75b7202cb4647407164d392f499
Alade Toheeb <aladetoheeb5@gmail.com>
2022-07-05 05:53:14 +0100

    fix: pycodestyle

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,6 +7,6 @@ Function inherit from
 
 def inherits_from(obj, a_class):
     """ Returns True if the obj inherits from the class"""
-    if type(obj) is  a_class:
+    if type(obj) is a_class:
         return False
     return isinstance(obj, a_class)
