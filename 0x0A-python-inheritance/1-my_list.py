d59b90091bcb1d078c775dfd4fd618ce88d173eb
Alade Toheeb <aladetoheeb5@gmail.com>
2022-07-05 05:17:36 +0100

    Add: documentation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Module 1-my_list
+Class Mylist
+"""
+
+
 class MyList(list):
+    """ A class inheriting from the list class"""
     def print_sorted(self):
+        """ Returns a sorted list"""
         print(sorted(self))
 
