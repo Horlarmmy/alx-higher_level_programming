19b9e8e3f29c7b445c48f1fd4bcbfaf139136b0f
Toheeb <aladetoheeb5@gmail.com>
2022-07-11 22:09:07 +0100

    Add: area, display, __str__, setter validators

--- 0x0C-python-almost_a_circle/rectangle.py
+++ 0x0C-python-almost_a_circle/rectangle.py
@@ -32,6 +32,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """ The setter function for height"""
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
         self.__height = value
 
     @property
@@ -42,6 +46,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """ Sets a new value for the width"""
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
@@ -52,6 +60,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """ Sets a new value for the x position"""
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -62,4 +74,26 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """ Sets a new value for the y position"""
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """ Calculate the area of the rectangle """
+        return self.__width * self.__height
+
+    def display(self):
+        """ This prints the Rectangle with #"""
+        print("\n" * self.__y, end="")
+
+        for i in range(self.__height):
+            print(" " * self.__x, end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """ Prints the string representation of the Rectangle class"""
+        return "[Rectangle] ({}) {}/{} - {}/{}" .format(self.id, self.__x, self.__y, self.__width, self.__height)
