9090b11d4dca9f17a3a599bcc88bf78f6740a4eb
Alade Toheeb <aladetoheeb5@gmail.com>
2022-07-13 08:29:23 +0100

    Fix: instantation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -18,10 +18,10 @@ class Rectangle(Base):
             __y: position y
             id: id
         """
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
         super().__init__(id)
 
     @property
