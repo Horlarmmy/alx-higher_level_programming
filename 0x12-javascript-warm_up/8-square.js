8d898c78f4ff8fd7c9f0ba80fccb72ffaf7675d2
Toheeb <aladetoheeb5@gmail.com>
2022-09-06 00:57:18 +0100

    Fix: semistandard

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,8 +1,9 @@
-#!/usr/bin/nodelet i = parseInt(process.argv[2]);
-if (i == NaN) {
-  console.log("Missing size");
+#!/usr/bin/node
+let i = parseInt(process.argv[2]);
+if (Number.isNaN(i)) {
+  console.log('Missing size');
 } else {
-  for (let x = 0; x < i; x++ ){
-    console.log("X".repeat(i));
+  for (let x = 0; x < i; x++) {
+    console.log('X'.repeat(i));
   }
 }
