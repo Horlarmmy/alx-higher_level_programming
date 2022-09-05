031920fde691e6caf8a363bc894d8833e05bacce
Toheeb <aladetoheeb5@gmail.com>
2022-09-06 00:51:18 +0100

    Fix: semistandard

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
-#!/usr/bin/nodeconst langs = ["C is fun", "Python is cool", "JavaScript is amazing"];
+#!/usr/bin/node
+const langs = ["C is fun", "Python is cool", "JavaScript is amazing"];
 for (lang of langs) {
   console.log(lang);
 }
