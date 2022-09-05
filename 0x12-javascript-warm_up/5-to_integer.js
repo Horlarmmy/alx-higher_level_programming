395333c65811cf97e404a742830b554a6bdf8528
Toheeb <aladetoheeb5@gmail.com>
2022-09-06 00:50:13 +0100

    Fix

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,7 @@
-#!/usr/bin/nodeif (process.argv[2] === undefined) {
+#!/usr/bin/node
+if (process.argv[2] === undefined) {
   console.log('Not a number');
-  } else if (Number.isNaN(parseInt(process.argv[2]))) {
+} else if (Number.isNaN(parseInt(process.argv[2]))) {
   console.log('Not a number');
 } else {
   console.log(parseInt(process.argv[2]));
