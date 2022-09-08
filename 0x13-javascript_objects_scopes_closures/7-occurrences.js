c825c952c9926cf50782d59e8e5e501742eccf33
Toheeb <aladetoheeb5@gmail.com>
2022-09-08 03:12:22 +0100

    Add: function that count no of occurences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,10 @@
+exports.nbOccurences = function (list, searchElement) {
+  const s = searchElement;
+  let count = 0;
+  for (let index = 0; index < list.length; index++) {
+    if (list[index] === s) {
+      count++;
+    }
+  }
+  return count;
+};
