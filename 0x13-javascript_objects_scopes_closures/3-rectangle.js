dc36e7a8400e88aacf1d0f7536503d7ff4ffbbee
Toheeb <aladetoheeb5@gmail.com>
2022-09-08 02:17:43 +0100

    Fix: print()

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
-    this.width = w;
-    this.height = h;
+      this.width = w;
+      this.height = h;
     }
   }
-  
-  print() {
-    for (var i = 0; i < height.length; i++) {
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
     }
   }
