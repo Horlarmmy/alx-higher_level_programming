c8252fec9422de902ff9f719d6bb75ff4c039e5f
Horlarmmy <aladetoheeb5@gmail.com>
2022-10-06 11:58:36 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ Function that print out content of a file
 
 def read_file(filename=""):
     """ A function that prints out """
-    with open(filename, encoding='utf-8') as f:
+    with open(filename, 'r', encoding='utf-8') as f:
         content = f.read()
-        print(content)
+        print(content, end='')
