2b71e8c665879f7319f31bb4ab3fc311732284b5
Alade Toheeb <aladetoheeb5@gmail.com>
2022-10-09 11:14:20 +0100

    Loads from file

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,5 +9,5 @@ import json
 def load_from_json_file(filename):
     """ Function that loads the obj from json"""
     with open(filename, 'w',) as f:
-        my_obj = json.load(str(f))
+        my_obj = json.load(f)
     return my_obj
