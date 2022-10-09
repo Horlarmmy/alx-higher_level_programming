cdc9714dc81d900f5df99bc2a4cf5951f1912d70
Alade Toheeb <aladetoheeb5@gmail.com>
2022-10-09 10:57:15 +0100

    Fix: Saves an object to json

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -8,6 +8,6 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """ Function that returns saves the obj in a jsonnfile"""
-    ans = json.loads(my_obj)
+    ans = json.loads(str(my_obj))
     with open(filename, 'w',) as f:
-        f.write(str(ans))
+        f.write(ans)
